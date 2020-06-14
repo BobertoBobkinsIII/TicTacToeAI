6d32a6928f2d9de97cf8f4aeb084edc3b67da46e
Mr.ML <50548791+BobertoBobkinsIII@users.noreply.github.com>
2020-06-14 18:03:03 -0400

    Should I implement epsillon

--- Game.py
+++ Game.py
@@ -22,14 +22,17 @@ class Game():
               or np.any(np.all(p, axis=1))       # --
               or np.all(np.diag(p))              # \
               or np.all(np.diag(np.fliplr(p))))
-    def showBoard(self):
+    def showBoard(self, game):
         board = [[' ', 'X', 'O'][idx] for i, idx in enumerate(self.board.astype(int))]
         return """\
+GAME:{}
  {} | {} | {}
 ---+---+---
  {} | {} | {}
 ---+---+---
- {} | {} | {} """.format(*board)
+ {} | {} | {}
+
+ """.format(game, *board)
 
 
     def giveReward(self, player):
@@ -48,11 +51,15 @@ class Agent():
         self.alpha = 0.1
         self.gamma = .95
     def policy(self, state):
-        if state not in self.qTable:
-            self.qTable[str(state)] = np.zeros(shape=(9,))
+        if str(state) not in self.qTable:
+            self.qTable[str(state)] =  np.zeros(shape=(9,))
         x = self.qTable[str(state)]
         if state.board[np.argmax(x)] != 0:
-            x[np.argmax(x)] = (np.min(x) - 1)
+            x[np.argmax(x)] = ((np.min(x))-1)
+        while state.board[np.argmax(x)] !=  0:
+            x[np.argmax(x)] = ((np.min(x))-1)
+        return np.argmax(x)
+
 
     def update(self, board_state, action, new_board_state, new_done, new_reward):
         if str(board_state) not in self.qTable:
@@ -63,29 +70,32 @@ class Agent():
             self.qTable[str(board_state)][action] = ((1 - self.alpha)* self.qTable[str(board_state)][action]+self.alpha * self.gamma * np.max(self.policy(new_board_state)))
 a = Agent(1)
 b = Agent(2)
-for i in range(10):
+a.qTable[str(np.zeros(shape=(9,)))] = np.zeros(shape=(9,))
+b.qTable[str(np.zeros(shape=(9,)))] = np.zeros(shape=(9,))
+for i in range(500):
 
     game = Game()
     done = False
-    a.qTable[str(game)] = np.zeros(shape=(9,))
-    b.qTable[str(game)] = np.zeros(shape=(9,))
-    print(a.qTable)
+    if i % 10 == 0:
+        print(b.qTable)
     while not done:
-        #if i+1 % 1 == 0:
-        print(game.showBoard())
-        time.sleep(2)
+        if i % 20 == 0:
+            print(game.showBoard(i+1))
+            time.sleep(2)
         oldState = game
-        aMove = a.policy(game)
+        aMove = a.policy(oldState)
         game.step(1, aMove)
         done, reward = game.giveReward(1)
-        print(str(oldState))
-        a.update(str(oldState), aMove, str(game), done, reward)
-        if game != done:
-            if i+1 % 1 == 0:
-                print(game.showBoard())
+        a.update(oldState, aMove, game, done, reward)
+        if not done:
+            if i % 20 == 0:
+                print(game.showBoard(i+1))
                 time.sleep(2)
             oldState = game.board
             bmove = b.policy(game)
             game.step(2, bmove)
             done, reward = game.giveReward(2)
             a.update(oldState, bmove, game, done, reward)
+        if i % 20 == 0:
+            print(game.showBoard(i+1))
+            time.sleep(2)
